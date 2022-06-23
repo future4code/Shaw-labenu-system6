59bd9196a69704e02befd579cb1edf42bd83d839
Matheus Augusto Moura de Lima <61365431+Mouramattheus@users.noreply.github.com>
2022-06-23 12:14:46 -0300

    endpoints-turma-finalizados

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Response,Request } from "express";
 import { app } from "./app";
+import createTurma from "./endpoint/CreateTurma";
+import getAllTurma from "./endpoint/GetTurma";
+import updateModulo from "./endpoint/UpdateTurma";
 
 
+
+app.get("/turma",getAllTurma)
+app.post("/create-turma",createTurma)
+app.put("/updateModel/:id", updateModulo)
