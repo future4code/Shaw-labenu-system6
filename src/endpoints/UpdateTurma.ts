6359774af974a0885b2dff9e213eea23310b23fb
Matheus Augusto Moura de Lima <61365431+Mouramattheus@users.noreply.github.com>
2022-06-24 11:16:31 -0300

    validacoes-turma

--- src/endpoints/UpdateTurma.ts
+++ src/endpoints/UpdateTurma.ts
@@ -7,12 +7,23 @@ export async function updateModulo(req: Request, res: Response) {
         const { id } = req.params
         const { modulo } = req.body
 
+        if (!id || !modulo) {
+            throw new Error("Dados em falta para atualizar o modulo!")
+        }
+
         const update = new TurmaDataBase()
         await update.update(modulo, id)
         res.status(202).send("Módulo Foi atualizado!")
 
     } catch (error: any) {
-        res.status(500).end("Error No Atualização do módulo")
+        switch (error.message) {
+            case "Dados em falta para atualizar o modulo!":
+                break;
+            default:
+                res.status(500).end("Error No Atualização do módulo")
+                break;
+        }
+        res.send(error.message)
 
     }
 
